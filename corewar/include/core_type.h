37fd038776fc41c6ea4fc91cd0d510aabb205a3b
Ambroise <ambroise.jacquemet-ramirez-vega@epitech.eu>
2023-05-15 11:00:35 +0200

    feat: :wrench: add .h header

---
+++ corewar/include/core_type.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2023
+** Corewar
+** File description:
+** core_type
+*/
+
+#ifndef CORE_TYPE_H_
+    #define CORE_TYPE_H_
+
+typedef char octet_t;
+
+#endif /* !CORE_TYPE_H_ */
