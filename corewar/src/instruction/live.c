102b2189c0e35bc43ff8acc1fe847883d89dea16
Etienne Namur <etienne.namur@epitech.eu>
2023-05-26 13:48:53 +0200

    refactor: new folder for tests

--- corewar/src/instruction/live.c
+++ corewar/src/instruction/live.c
@@ -11,19 +11,19 @@
 #include "corewar_proto.h"
 #include "my.h"
 
-static void diplay_alive(char const *name, int id)
+static void display_alive(char const *name, int id)
 {
     my_putstr("le joueur ");
     my_putnbr(id);
     my_putstr(" (");
     my_putstr(name);
-    my_putstr(") est en vie.");
+    my_putstr(") est en vie.\n");
 }
 
 int live(octet_t memory[MEM_SIZE], process_t *process)
 {
     int i = process->PC + 1;
-    int player_number;
+    int player_number = 0;
     corewar_t *core = corewar_store();
 
     if (process->wait < 10)
@@ -31,7 +31,7 @@ int live(octet_t memory[MEM_SIZE], process_t *process)
     player_number = GET_MEM_DIR(memory, &i);
     for (int j = 0; j < core->nb_original_prog; ++j) {
         if (core->all_names[j].id == player_number) {
-            diplay_alive(core->all_names[j].name, core->all_names[j].id);
+            display_alive(core->all_names[j].name, core->all_names[j].id);
         }
     }
     for (int i = 0; i < core->nb_processes; ++i) {
